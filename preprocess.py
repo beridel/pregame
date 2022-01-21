2bd55e671831bd804fd69439022325f4de38ead4
Caroline Mouchon <mouchonc@mit.edu>
2022-01-21 15:59:47 -0500

    Add "create_file_list" & "reading_headers" functions
    create_file_list generate a list of files in a path.
    reading_headers read only headers information from a list of stream files

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,16 @@
-hello
+import numpy as np
+import obspy as obs
+import os
+
+def create_file_list(path):
+    file_list = os.listdir(path)
+    print('Finished creating file list for {} total files'.format(len(file_list)))
+
+def reading_headers(file_list, station, ):
+    header = obs.Stream() #set header as an empty stream object
+    for file in files_list
+        str = obs.read(file, headonly=True)
+        for trace in str.traces:
+            trace.file = file
+        header.extend(str)
+    print('Finished reading headers')
